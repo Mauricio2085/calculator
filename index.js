8e8491d2846e367aa60cf40638d9121a0e3b2951
mauricio ocampo marulanda <maoca2085@gmail.com>
2022-11-08 19:28:05 -0500

    Se crean constantes en el index.js

--- index.js
+++ index.js
@@ -1,15 +1,85 @@
+let screen = document.querySelector('.screen');
 const one = document.querySelector('#one');
+const two = document.querySelector('#two');
+const three = document.querySelector('#three');
+const four = document.querySelector('#four');
+const five = document.querySelector('#five');
+const six = document.querySelector('#six');
+const seven = document.querySelector('#seven');
+const eihgt = document.querySelector('#eight');
+const nine = document.querySelector('#nine');
+const zero = document.querySelector('#zero');
+const equal = document.querySelector('#equal');
+const coma = document.querySelector('#coma');
 
-let screen = [];
+
 one.addEventListener('click', addOne);
+two.addEventListener('click',addTwo);
+three.addEventListener('click', addThree);
+four.addEventListener('click', addFour);
+five.addEventListener('click', addFive);
+six.addEventListener('click', addSix);
+seven.addEventListener('click', addSeven);
+eihgt.addEventListener('click', addEight);
+nine.addEventListener('click', addNine);
+zero.addEventListener('click', addZero);
+coma.addEventListener('click', addComa);
+
 
 function addOne () {
-   let readScreen = document.querySelector('.screen');
+   screen.textContent = screen.textContent + '1';
    console.log(screen);
-   screen.push(1);
-   screen.innerText = screen;
-
 }
+
+function addTwo () {
+    screen.textContent = screen.textContent + '2';
+    console.log(screen);
+ }
+
+ function addThree () {
+    screen.textContent = screen.textContent + '3';
+    console.log(screen);
+ }
+
+ function addFour () {
+    screen.textContent = screen.textContent + '4';
+    console.log(screen);
+ }
+
+ function addFive () {
+    screen.textContent = screen.textContent + '5';
+    console.log(screen);
+ }
+
+ function addSix () {
+    screen.textContent = screen.textContent + '6';
+    console.log(screen);
+ }
+
+ function addSeven () {
+    screen.textContent = screen.textContent + '7';
+    console.log(screen);
+ }
+
+ function addEight () {
+    screen.textContent = screen.textContent + '8';
+    console.log(screen);
+ }
+
+ function addNine () {
+    screen.textContent = screen.textContent + '9';
+    console.log(screen);
+ }
+
+ function addZero () {
+    screen.textContent = screen.textContent + '0';
+    console.log(screen);
+ }
+
+ function addComa () {
+    screen.textContent = screen.textContent + '.';
+    console.log(screen);
+ }
 
 
 function addOperator (number1, number2) {
